93e236f588b3f478c89b17a6f718746378176785
Robin Ward <robin.ward@gmail.com>
2014-04-03 14:51:11 -0400

    Bump version

--- wp-discourse.php
+++ wp-discourse.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP-Discourse
 Description: Allows you to publish your posts to a Discourse instance and view top Discourse comments on your blog
-Version: 0.5.5
+Version: 0.5.6
 Author: Sam Saffron, Robin Ward
 Author URI: https://github.com/discourse/wp-discourse/
 */
