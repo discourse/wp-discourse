c94ed617e5fdd6744afef8f4c40b3575b365229e
Arpit Jalan <arpit@techapj.com>
2014-06-23 21:47:11 +0530

    FEATURE: Use standard WordPress style, support default themes

--- comments.php
+++ comments.php
@@ -36,30 +36,39 @@
 
 ?>
 
-<div id="comments">
+<div id="comments" class="comments-area">
 <?php if(count($discourse_info->posts) > 0) { ?>
-  <h2 id="comments-title"><?php echo $comments_title ?></h2>
-  <ol class="commentlist">
+  <h2 class="comments-title"><?php echo $comments_title ?></h2>
+  <ol class="comment-list">
     <?php foreach($discourse_info->posts as &$post) { ?>
-    <li class="comment">
-  		<div class="comment-author vcard">
-        <img alt="" src="<?php Discourse::avatar($post->avatar_template,64) ?>" class="avatar avatar-64 photo avatar-default" height="64" width="64">
-          <a href="<?php Discourse::homepage($options['url'],$post) ?>" rel="external" class="url"><?php echo ($show_fullname ? $post->name : $post->username) ?></a>
-          <br/>
-          <time pubdate="" datetime="<?php echo $post->created_at ?>"><?php echo mysql2date(get_option('date_format'), $post->created_at)?></time>
-      </div>
-      <div class="comment-content"><?php echo $post->cooked ?></div>
+
+    <li class="comment even thread-even depth-1">
+      <article class="comment-body">
+        <footer class="comment-meta">
+          <div class="comment-author vcard">
+            <img alt="" src="<?php Discourse::avatar($post->avatar_template,64) ?>" class="avatar avatar-64 photo avatar-default" height="64" width="64">
+            <b class="fn"><a href="<?php Discourse::homepage($options['url'],$post) ?>" rel="external" class="url"><?php echo ($show_fullname ? $post->name : $post->username) ?></a></b>
+            <span class="says">says:</span>
+          </div><!-- .comment-author -->
+
+          <div class="comment-metadata">
+            <time pubdate="" datetime="<?php echo $post->created_at ?>"><?php echo mysql2date(get_option('date_format'), $post->created_at)?></time>
+          </div><!-- .comment-metadata -->
+        </footer><!-- .comment-meta -->
+        <div class="comment-content"><?php echo $post->cooked ?></div><!-- .comment-content -->
+      </article><!-- .comment-body -->
     </li>
+
     <?php } ?>
   </ol>
 
 <?php } ?>
 
-  <div class="respond">
-      <h3 class="reply-title"><a href="<?php echo $permalink ?>"><?php echo $link_text ?></a> at <?php echo $discourse_url_name ?></h3>
+  <div class="respond" class="comment-respond">
+      <h3 id="reply-title" class="comment-reply-title"><a href="<?php echo $permalink ?>"><?php echo $link_text ?></a> at <?php echo $discourse_url_name ?></h3>
       <?php if(count($discourse_info->posts) > 0 || $more_replies > 0) { ?>
       <p class='more-replies'><?php echo $more_replies ?></p>
-      <p>
+      <p class="comment-reply-title">
         <?php foreach($discourse_info->participants as &$participant) { ?>
           <img alt="" src="<?php Discourse::avatar($participant->avatar_template,25) ?>" class="avatar avatar-25 photo avatar-default" height="25" width="25">
         <?php } ?>
