a4a810ed3daa8eb0f208ae58aa797d6ad5c03ebd
Arpit Jalan <arpit@techapj.com>
2014-08-26 13:01:56 +0530

    minor fix

--- js/discourse.js
+++ js/discourse.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function() {
   youtube_id = jQuery(".lazyYT").data('youtube-id');
-  parameter = jQuery(".lazyYT").data('parameters');
   link = "https://www.youtube.com/watch?v=" + youtube_id;
   jQuery(".lazyYT").replaceWith("<a href='" + link + "'>" + link + "</a>");
 });
