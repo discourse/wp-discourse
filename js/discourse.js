d5bf744cc34b7bf20cc98364ee11ae68a9167486
Ben Word <ben@benword.com>
2015-01-04 22:36:17 -0600

    Move JS from separate file to inline

--- js/discourse.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function() {
-  youtube_id = jQuery(".lazyYT").data('youtube-id');
-  link = "https://www.youtube.com/watch?v=" + youtube_id;
-  jQuery(".lazyYT").replaceWith("<a href='" + link + "'>" + link + "</a>");
-});
