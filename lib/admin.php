1475d670102167b6f6c10f65b4126dd582607d03
Ionut Staicu <github@iamntz.com>
2015-02-25 08:57:33 +0200

    whitespaces should be stripped only on strings

--- lib/admin.php
+++ lib/admin.php
@@ -284,7 +284,7 @@ class DiscourseAdmin {
 
   function discourse_validate_options( $inputs ) {
     foreach ( $inputs as $key => $input ) {
-      $inputs[ $key ] = trim( $input );
+      $inputs[ $key ] = gettype( $input ) == 'string' ? trim( $input ) : $input;
     }
 
     $inputs['url'] = untrailingslashit( $inputs['url'] );
