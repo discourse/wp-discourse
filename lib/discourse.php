8eba951e2bc185aaa473cfa3ccd36e4995111209
Jake Rossi <jake.rossi@sitepoint.com>
2015-08-18 02:25:46 +0000

    Fix avatar returning the wrong url

--- lib/discourse.php
+++ lib/discourse.php
@@ -10,7 +10,7 @@ class Discourse {
   public static function avatar( $template, $size ) {
     $options = self::get_plugin_options();
     $template_http = parse_url($template);
-    $template = $options["url"] . $template_http["path"];
+    $template = $template_http["scheme"] . "://" . $template_http["host"] . $template_http["path"];
     return str_replace( "{size}", $size, $template );
   }
 
