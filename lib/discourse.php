69b86a63109abab8b8fcac902912db8ba4d49b07
Grexy <Grexy@users.noreply.github.com>
2015-11-16 13:00:51 -0500

    replace fucntion get_avatar_url
    As of WordPress 4.2, there is now has a built in get_avatar_url function.  Removing custom function and rewritten to use WordPress function.

--- lib/discourse.php
+++ lib/discourse.php
@@ -196,6 +196,8 @@ class Discourse {
         // Current user info
         global $current_user;
         get_currentuserinfo();
+        $current_user_id = get_current_user_id();
+        $avatar = get_avatar_url($current_user_id);
 
         // Map information
         $params = array(
@@ -205,7 +207,7 @@ class Discourse {
           'email' => $current_user->user_email,
           'about_me' => $current_user->description,
           'external_id' => $current_user->ID,
-          'avatar_url' => self::get_avatar_url($current_user->ID)
+          'avatar_url' => $avatar
         );
 
         // Build login string
