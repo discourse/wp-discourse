abfa5d73b3b4cf8067f0a1e2a5139f199f9696b7
Adam Capriola <adam.capriola@gmail.com>
2016-02-20 13:09:33 -0500

    Create comments.js

---
+++ js/comments.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function() {
+	jQuery('.lazyYT').each(function() {
+		var id = jQuery(this).data('youtube-id'),
+			url = 'https://www.youtube.com/watch?v=' + id;
+		jQuery(this).replaceWith('<a href="' + url + '">' + url + '</a>');
+	});
+	jQuery('a.mention').each(function() {
+		jQuery(this).attr('href', discourse_url + jQuery(this).attr('href'));
+	});
+});
