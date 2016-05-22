bbc97fac17638543e2df694723f8108da4924145
scossar <scossar3@gmail.com>
2016-05-22 14:22:17 -0700

    hook into redirect filter

--- lib/plugin-support/woocommerce_support.php
+++ lib/plugin-support/woocommerce_support.php
@@ -14,19 +14,15 @@ class WoocommerceSupport {
   }
 
   private function __construct() {
-    add_action( 'woocommerce_login_form_end', array( $this, 'set_redirect' ) );
+    add_filter( 'woocommerce_login_redirect', array( $this, 'set_redirect' ) );
   }
-
-  // The WooCommerce login form handler (`WC_Form_Handler::process_login`) tries to set the
-  // after-login redirect path based on `$_POST['redirect']`. If that isn't set, it falls back to
-  // the 'myaccount' page permalink. This function sets a hidden 'redirect' field based on the
-  // request's 'redirect_to' value. Doing it this way allows the values for 'sso' and 'sig' to
-  // be passed to the redirect.
-  function set_redirect() {
+  
+  function set_redirect( $redirect ) {
     if ( array_key_exists( 'redirect_to', $_GET ) ) {
       $redirect = $_GET['redirect_to'];
-      echo '<input type="hidden" name="redirect" value="'. $redirect . '">';
+      return $redirect;
     }
+    return $redirect;
   }
 
   function get_comments_number_filter() {
