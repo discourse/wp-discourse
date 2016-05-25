7aa507607d8b295e97610a95ee7636ea44fce0fd
scossar <scossar3@gmail.com>
2016-05-25 10:38:04 -0700

    check if 'prodict' is in allowed_post_types

--- lib/plugin-support/woocommerce_support.php
+++ lib/plugin-support/woocommerce_support.php
@@ -7,9 +7,18 @@ class WooCommerceSupport {
 
   function __construct( \Discourse $discourse ) {
     $this->discourse = $discourse;
-    
+
     add_filter( 'woocommerce_login_redirect', array( $this, 'set_redirect' ) );
-    add_filter( 'woocommerce_product_review_count', array( $this->discourse, 'comments_number' ) );
+    add_action( 'init', array( $this, 'set_product_review_count_filter' ) );
+  }
+
+  // Only use the Discourse comments number if 'product' is in allowed post types.
+  function set_product_review_count_filter() {
+    $options = get_option('discourse');
+    if ( array_key_exists( 'allowed_post_types', $options ) 
+         && in_array( 'product', $options['allowed_post_types'] ) ) {
+      add_filter( 'woocommerce_product_review_count', array( $this->discourse, 'comments_number' ) );
+    }
   }
 
   function set_redirect( $redirect ) {
