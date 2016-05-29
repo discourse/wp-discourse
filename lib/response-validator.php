3f3882c85d2a142ab85967c10021e6f84f5d4f3d
scossar <scossar3@gmail.com>
2016-05-28 23:28:57 -0700

    hmmm

--- lib/response-validator.php
+++ lib/response-validator.php
@@ -3,7 +3,8 @@ namespace WPDiscourse\ResponseValidator;
 
 class ResponseValidator {
   static protected $instance;
-  protected $connection_status;
+  
+  public $connection_status;
   
   public static function get_instance() {
     if ( null == self::$instance ) {
@@ -27,10 +28,10 @@ class ResponseValidator {
   }
   
   
-  function validate( $response, $set_status = true ) {
+  function validate( $response, $update_status = true ) {
     if ( is_wp_error( $response ) ) {
       error_log( $response->get_error_message() );
-      if ( $set_status ) {
+      if ( $update_status ) {
         var_dump('setting status wp error');
         $this->set_status( 0 );
       }
@@ -39,14 +40,14 @@ class ResponseValidator {
     } elseif ( wp_remote_retrieve_response_code( $response ) != 200 ) {
       $error_message = wp_remote_retrieve_response_code( $response );
       error_log( 'There has been a problem accessing your Discourse forum. Error Message: ' . $error_message );
-      if ( $set_status ) {
+      if ( $update_status ) {
         $this->set_status( 0 );
       }
       var_dump( 'setting status error' );
       return 0;
     }
     // valid response 
-    if ( $set_status ) {
+    if ( $update_status ) {
       $this->set_status( 1 );
     }
     var_dump('its good');
