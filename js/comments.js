66b728a84aa552287aabe489c018d895b0f23d74
scossar <scossar3@gmail.com>
2016-06-10 13:33:51 -0700

    don't send email notifications

--- js/comments.js
+++ js/comments.js
@@ -1,10 +1,16 @@
-jQuery(document).ready(function () {
-    jQuery('.lazyYT').each(function () {
-        var id = jQuery(this).data('youtube-id'),
-            url = 'https://www.youtube.com/watch?v=' + id;
-        jQuery(this).replaceWith('<a href="' + url + '">' + url + '</a>');
-    });
-    jQuery('a.mention').each(function () {
-        jQuery(this).attr('href', discourse.url + jQuery(this).attr('href'));
-    });
+/**
+ * Fixes Discourse oneboxes and mention links for display on WordPress.
+ *
+ * @package WPDiscourse
+ */
+
+jQuery( document ).ready(function () {
+	jQuery( '.lazyYT' ).each(function () {
+		var id = jQuery( this ).data( 'youtube-id' ),
+			url = 'https://www.youtube.com/watch?v=' + id;
+		jQuery( this ).replaceWith( '<a href="' + url + '">' + url + '</a>' );
+	});
+	jQuery( 'a.mention' ).each(function () {
+		jQuery( this ).attr( 'href', discourse.url + jQuery( this ).attr( 'href' ) );
+	});
 });
--- wp-discourse.php
+++ wp-discourse.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
 Plugin Name: WP-Discourse
 Description: Use Discourse as a community engine for your WordPress blog
 Version: 0.7.0
@@ -9,23 +9,26 @@ Domain Path: /languages
 Author URI: https://github.com/discourse/wp-discourse
 Plugin URI: https://github.com/discourse/wp-discourse
 GitHub Plugin URI: https://github.com/discourse/wp-discourse
-*/
-/*  Copyright 2014 Civilized Discourse Construction Kit, Inc (team@discourse.org)
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+@package WPDiscourse
+ */
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General Public License for more details.
+/**  Copyright 2014 Civilized Discourse Construction Kit, Inc (team@discourse.org)
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-*/
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 define( 'WPDISCOURSE_PATH', plugin_dir_path( __FILE__ ) );
 define( 'WPDISCOURSE_URL', plugins_url( '', __FILE__ ) );
