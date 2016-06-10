effa2710a36ae588fa3c88e67f5f0f7a51f1bd7a
scossar <scossar3@gmail.com>
2016-06-10 15:53:12 -0700

    rename file

--- wp-discourse.php
+++ wp-discourse.php
@@ -39,12 +39,12 @@ require_once( __DIR__ . '/lib/settings-validator.php' );
 require_once( __DIR__ . '/lib/response-validator.php' );
 require_once( __DIR__ . '/lib/admin.php' );
 require_once( __DIR__ . '/lib/sso.php' );
-require_once( __DIR__ . '/lib/plugin-support/woocommerce_support.php' );
+require_once( __DIR__ . '/lib/plugin-support/woocommerce-support.php' );
 
 $discourse_response_validator = WPDiscourse\ResponseValidator\ResponseValidator::get_instance();
 $discourse_settings_validator = new WPDiscourse\Validator\SettingsValidator();
 $discourse                    = new Discourse( $discourse_response_validator );
 $discourse_admin              = new DiscourseAdmin( $discourse_response_validator );
-$woocommerce_support          = new WPDiscourse\PluginSupport\WooCommerceSupport( $discourse );
+$woocommerce_support          = new WPDiscourse\PluginSupport\WooCommerceSupport();
 
 register_activation_hook( __FILE__, array( $discourse, 'install' ) );
