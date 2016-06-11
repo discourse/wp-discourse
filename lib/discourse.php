bb311247102aefd32a8e57759a7b8cdc744d3785
scossar <scossar3@gmail.com>
2016-06-11 16:20:04 -0700

    remove sso code

--- lib/discourse.php
+++ lib/discourse.php
@@ -70,15 +70,12 @@ class Discourse {
 		// replace comments with discourse comments
 		add_filter( 'comments_number', array( $this, 'comments_number' ) );
 		add_filter( 'comments_template', array( $this, 'comments_template' ), 20, 1 );
-//		add_filter( 'query_vars', array( $this, 'sso_add_query_vars' ) );
 		add_filter( 'login_url', array( $this, 'set_login_url' ), 10, 2 );
 
 		add_action( 'wp_enqueue_scripts', array( $this, 'discourse_comments_js' ) );
-
 		add_action( 'save_post', array( $this, 'save_postdata' ) );
 		add_action( 'xmlrpc_publish_post', array( $this, 'xmlrpc_publish_post_to_discourse' ) );
 		add_action( 'transition_post_status', array( $this, 'publish_post_to_discourse' ), 10, 3 );
-//		add_action( 'parse_query', array( $this, 'sso_parse_request' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_styles' ) );
 	}
 
@@ -132,107 +129,7 @@ class Discourse {
 			}
 		}
 	}
-
-//	function sso_add_query_vars( $vars ) {
-//		$vars[] = "sso";
-//		$vars[] = "sig";
-//
-//		return $vars;
-//	}
-
-	// SSO Request Processing from Adam Capirola : https://gist.github.com/adamcapriola/11300529
-/*	function sso_parse_request( $wp ) {
-		$discourse_options = self::get_plugin_options();
-
-		// sync logout from Discourse to WordPress from Adam Capirola : https://meta.discourse.org/t/wordpress-integration-guide/27531
-		// to make this work, enter a URL of the form "http://my-wp-blog.com/?request=logout" in the "logout redirect"
-		// field in your Discourse admin
-		if ( isset( $discourse_options['enable-sso'] ) &&
-		     intval( $discourse_options['enable-sso'] ) == 1 &&
-		     isset( $_GET['request'] ) && $_GET['request'] == 'logout'
-		) {
-
-			wp_logout();
-			wp_redirect( $discourse_options['url'] );
-			exit;
-		}
-		// end logout processing
-
-		// only process requests with "my-plugin=ajax-handler"
-		if ( isset( $discourse_options['enable-sso'] ) &&
-		     intval( $discourse_options['enable-sso'] ) == 1 &&
-		     array_key_exists( 'sso', $wp->query_vars ) &&
-		     array_key_exists( 'sig', $wp->query_vars )
-		) {
-
-			// Not logged in to WordPress, redirect to WordPress login page with redirect back to here
-			if ( ! is_user_logged_in() ) {
-
-				// Preserve sso and sig parameters
-				$redirect = add_query_arg( null, null );
-
-				// Change %0A to %0B so it's not stripped out in wp_sanitize_redirect
-				$redirect = str_replace( '%0A', '%0B', $redirect );
-
-				// Build login URL
-				$login = wp_login_url( esc_url_raw( $redirect ) );
-
-				// Redirect to login
-				wp_redirect( $login );
-				exit;
-			} else {
-
-				// Check for helper class
-				if ( ! class_exists( 'Discourse_SSO' ) ) {
-					// Error message
-					echo( 'Helper class is not properly included.' );
-					exit;
-				}
-
-				// Payload and signature
-				$payload = $wp->query_vars['sso'];
-				$sig     = $wp->query_vars['sig'];
-
-				// Change %0B back to %0A
-				$payload = urldecode( str_replace( '%0B', '%0A', urlencode( $payload ) ) );
-
-				// Validate signature
-				$sso_secret = $discourse_options['sso-secret'];
-				$sso        = new Discourse_SSO( $sso_secret );
-				if ( ! ( $sso->validate( $payload, $sig ) ) ) {
-					// Error message
-					echo( 'Invalid request.' );
-					exit;
-				}
-
-				// Nonce
-				$nonce = $sso->get_nonce( $payload );
-
-				// Current user info
-				$current_user = wp_get_current_user();
-
-				// Map information
-				$params = array(
-					'nonce'       => $nonce,
-					'name'        => $current_user->display_name,
-					'username'    => $current_user->user_login,
-					'email'       => $current_user->user_email,
-					'about_me'    => $current_user->description,
-					'external_id' => $current_user->ID,
-					'avatar_url'  => get_avatar_url( get_current_user_id() )
-				);
-
-				// Build login string
-				$q = $sso->build_login_string( $params );
-
-				// Redirect back to Discourse
-				wp_redirect( $discourse_options['url'] . '/session/sso_login?' . $q );
-				exit;
-			}
-		}
-	}
-*/
-
+	
 	static function convert_relative_img_src_to_absolute( $url, $content ) {
 		if ( preg_match( "/<img\s*src\s*=\s*[\'\"]?(https?:)?\/\//i", $content ) ) {
 			return $content;
