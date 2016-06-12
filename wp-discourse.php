3ffcb42fbece50f8ad268e671f68279f0f8d452a
scossar <scossar3@gmail.com>
2016-06-12 14:42:59 -0700

    reformat

--- wp-discourse.php
+++ wp-discourse.php
@@ -1,33 +1,33 @@
 <?php
 /**
-Plugin Name: WP-Discourse
-Description: Use Discourse as a community engine for your WordPress blog
-Version: 0.7.0
-Author: Sam Saffron, Robin Ward
-Text Domain: wp-discourse
-Domain Path: /languages
-Author URI: https://github.com/discourse/wp-discourse
-Plugin URI: https://github.com/discourse/wp-discourse
-GitHub Plugin URI: https://github.com/discourse/wp-discourse
-
-@package WPDiscourse
+ * Plugin Name: WP-Discourse
+ * Description: Use Discourse as a community engine for your WordPress blog
+ * Version: 0.7.0
+ * Author: Sam Saffron, Robin Ward
+ * Text Domain: wp-discourse
+ * Domain Path: /languages
+ * Author URI: https://github.com/discourse/wp-discourse
+ * Plugin URI: https://github.com/discourse/wp-discourse
+ * GitHub Plugin URI: https://github.com/discourse/wp-discourse
+ *
+ * @package WPDiscourse
  */
 
 /**  Copyright 2014 Civilized Discourse Construction Kit, Inc (team@discourse.org)
-
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 define( 'WPDISCOURSE_PATH', plugin_dir_path( __FILE__ ) );
@@ -49,9 +49,9 @@ $woocommerce_support          = new WPDiscourse\PluginSupport\WooCommerceSupport
 $discourse_settings_validator = new WPDiscourse\Validator\SettingsValidator();
 $discourse                    = new WPDiscourse\Discourse\Discourse();
 $discourse_admin              = new DiscourseAdmin();
-$discourse_publisher = new WPDiscourse\DiscoursePublish\DiscoursePublish();
-$discourse_comment = new WPDiscourse\DiscourseComment\DiscourseComment();
-$discourse_sso = new WPDiscourse\DiscourseSSO\DiscourseSSO();
-$discourse_publish_metabox = new WPDiscourse\MetaBox\MetaBox();
+$discourse_publisher          = new WPDiscourse\DiscoursePublish\DiscoursePublish();
+$discourse_comment            = new WPDiscourse\DiscourseComment\DiscourseComment();
+$discourse_sso                = new WPDiscourse\DiscourseSSO\DiscourseSSO();
+$discourse_publish_metabox    = new WPDiscourse\MetaBox\MetaBox();
 
 register_activation_hook( __FILE__, array( $discourse, 'install' ) );
