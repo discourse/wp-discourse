22b51d9affbdb82caebaecfa380385ff27b07454
scossar <scossar3@gmail.com>
2016-06-12 17:20:35 -0700

    set discourse as global variable

--- js/comments.js
+++ js/comments.js
@@ -1,16 +1,17 @@
+/* globals discourse */
 /**
  * Fixes Discourse oneboxes and mention links for display on WordPress.
  *
  * @package WPDiscourse
  */
 
-jQuery( document ).ready(function () {
-	jQuery( '.lazyYT' ).each(function () {
+jQuery( document ).ready(function() {
+	jQuery( '.lazyYT' ).each(function() {
 		var id = jQuery( this ).data( 'youtube-id' ),
 			url = 'https://www.youtube.com/watch?v=' + id;
 		jQuery( this ).replaceWith( '<a href="' + url + '">' + url + '</a>' );
 	});
-	jQuery( 'a.mention' ).each(function () {
+	jQuery( 'a.mention' ).each(function() {
 		jQuery( this ).attr( 'href', discourse.url + jQuery( this ).attr( 'href' ) );
 	});
 });
