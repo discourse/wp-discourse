7288231809f351d4a08663d0dd9ece80bcfd0ce5
scossar <scossar3@gmail.com>
2016-06-16 17:28:27 -0700

    setup unit tests

---
+++ tests/test-sample.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package 
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
+
