a63f0d4fe4dabb1b1827cb0880a8e470444a6419
scossar <scossar3@gmail.com>
2016-06-22 17:33:20 -0700

    add php-mock-phpunit for mocking wordpress functions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,8 @@
  * @package wp-discourse
  */
 
+require_once( dirname( __DIR__) . '/vendor/autoload.php' );
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
