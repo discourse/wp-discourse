89444dff96c4cae60db834726f5235c4dcdc3858
scossar <scossar3@gmail.com>
2016-07-16 12:10:04 -0700

    Inlude and instantiate WordPressEmailVerification class

--- wp-discourse.php
+++ wp-discourse.php
@@ -39,6 +39,7 @@ require_once( __DIR__ . '/lib/discourse.php' );
 require_once( __DIR__ . '/lib/settings-validator.php' );
 require_once( __DIR__ . '/lib/admin.php' );
 require_once( __DIR__ . '/lib/sso.php' );
+require_once( __DIR__ . '/lib/wordpress-email-verification.php' );
 require_once( __DIR__ . '/lib/discourse-sso.php' );
 require_once( __DIR__ . '/lib/discourse-publish.php' );
 require_once( __DIR__ . '/lib/discourse-comment.php' );
@@ -50,7 +51,8 @@ $discourse                    = new WPDiscourse\Discourse\Discourse();
 $discourse_admin              = new WPDiscourse\DiscourseAdmin\DiscourseAdmin();
 $discourse_publisher          = new WPDiscourse\DiscoursePublish\DiscoursePublish();
 $discourse_comment            = new WPDiscourse\DiscourseComment\DiscourseComment();
-$discourse_sso                = new WPDiscourse\DiscourseSSO\DiscourseSSO();
+$wordpress_email_verifier     = new WPDiscourse\WordPressEmailVerification\WordPressEmailVerification( 'discourse_email_verification_key', 'discourse', 'wp-discourse' );
+$discourse_sso                = new WPDiscourse\DiscourseSSO\DiscourseSSO( $wordpress_email_verifier );
 $discourse_publish_metabox    = new WPDiscourse\MetaBox\MetaBox();
 $discourse_woocommerce = new WPDiscourse\PluginSupport\WooCommerceSupport();
 
