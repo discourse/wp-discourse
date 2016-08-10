7699b91c8869af19ae1160306b74d561e87d2098
scossar <scossar3@gmail.com>
2016-08-10 15:21:45 -0700

    Update version to 0.9.4

--- wp-discourse.php
+++ wp-discourse.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP-Discourse
  * Description: Use Discourse as a community engine for your WordPress blog
- * Version: 0.9.3
+ * Version: 0.9.4
  * Author: Sam Saffron, Robin Ward
  * Text Domain: wp-discourse
  * Domain Path: /languages
