a004ed6742da446d8f8a5078f1d1bab75bacd34b
scossar <scossar3@gmail.com>
2016-09-20 15:02:40 -0700

    Remove WooCommerce class

--- wp-discourse.php
+++ wp-discourse.php
@@ -44,7 +44,6 @@ require_once( __DIR__ . '/lib/discourse-sso.php' );
 require_once( __DIR__ . '/lib/discourse-publish.php' );
 require_once( __DIR__ . '/lib/discourse-comment.php' );
 require_once( __DIR__ . '/lib/meta-box.php' );
-require_once( __DIR__ . '/lib/plugin-support/woocommerce-support.php' );
 
 $discourse_settings_validator = new WPDiscourse\Validator\SettingsValidator();
 $discourse                    = new WPDiscourse\Discourse\Discourse();
@@ -54,6 +53,5 @@ $discourse_comment            = new WPDiscourse\DiscourseComment\DiscourseCommen
 $wordpress_email_verifier     = new WPDiscourse\WordPressEmailVerification\WordPressEmailVerification( 'discourse_email_verification_key', 'discourse', 'wp-discourse' );
 $discourse_sso                = new WPDiscourse\DiscourseSSO\DiscourseSSO( $wordpress_email_verifier );
 $discourse_publish_metabox    = new WPDiscourse\MetaBox\MetaBox();
-$discourse_woocommerce        = new WPDiscourse\PluginSupport\WooCommerceSupport();
 
 register_activation_hook( __FILE__, array( $discourse, 'install' ) );
