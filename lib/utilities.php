cab6c8f3d54748e8d52d929c62a806bb68e08286
scossar <scossar3@gmail.com>
2016-09-24 19:03:54 -0700

    Move template functions to their own class

--- lib/utilities.php
+++ lib/utilities.php
@@ -83,52 +83,6 @@ class Utilities {
 	}
 
 	/**
-	 * Returns the user's Discourse homepage.
-	 *
-	 * @param string $url The base URL of the Discourse forum.
-	 * @param object $post The Post object.
-	 *
-	 * @return string
-	 */
-	public static function homepage( $url, $post ) {
-		return $url . '/users/' . strtolower( $post->username );
-	}
-
-	/**
-	 * Substitutes the value for `$size` into the template.
-	 *
-	 * @param string $template The avatar template.
-	 * @param int $size The size of the avarar.
-	 *
-	 * @return mixed
-	 */
-	public static function avatar( $template, $size ) {
-		return str_replace( '{size}', $size, $template );
-	}
-
-	/**
-	 * Replaces relative image src with absolute.
-	 *
-	 * This function may not be required anymore.
-	 * See: https://meta.discourse.org/t/can-emoji-be-rendered-with-absolute-urls/47250
-	 *
-	 * @param string $url The base url of the forum.
-	 * @param string $content The content to be checked.
-	 *
-	 * @return mixed
-	 */
-	public static function convert_relative_img_src_to_absolute( $url, $content ) {
-		if ( preg_match( "/<img\s*src\s*=\s*[\'\"]?(https?:)?\/\//i", $content ) ) {
-			return $content;
-		}
-
-		$search  = '#<img src="((?!\s*[\'"]?(?:https?:)?\/\/)\s*([\'"]))?#';
-		$replace = "<img src=\"{$url}$1";
-
-		return preg_replace( $search, $replace, $content );
-	}
-
-	/**
 	 * Gets the Discourse categories.
 	 *
 	 * @return array|mixed|object|\WP_Error|WP_Error
@@ -174,25 +128,5 @@ class Utilities {
 		}
 
 		return $remote;
-	}
-
-	/**
-	 * This function allows string to pass through unsanitized when in the development environment.
-	 *
-	 * This is a temporary work around for handling protocol relative image src urls.
-	 * see http://wordpress.stackexchange.com/questions/232420/getting-wp-kses-post-to-handle-protocol-relative-image-src-urls-that-include-a-p.
-	 *
-	 * TODO: Find a better way to do this.
-	 *
-	 * @param string $string The string to be sanitized.
-	 *
-	 * @return mixed
-	 */
-	public static function sanitize_for_environment( $string ) {
-		if ( defined( 'WP_ENV' ) && 'development' === WP_ENV ) {
-			return $string;
-		} else {
-			return wp_kses_post( $string );
-		}
 	}
 }
