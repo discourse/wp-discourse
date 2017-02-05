684821e04bc88b562f648a9cda198f015b8b6edc
Ionut Staicu <github@iamntz.com>
2017-02-05 14:09:08 +0200

    Added nonce generated on click, not on render

--- lib/shortcodes/external-sso.php
+++ lib/shortcodes/external-sso.php
@@ -7,37 +7,33 @@
 
 use \WPDiscourse\Utilities\Utilities as DiscourseUtilities;
 
-/**
- * Gets a auth URL for discourse
- *
- * @param  array $options anchor, link.
- *
- * @return string.
- */
-function get_discourse_sso_url( $options = array() ) {
+add_filter( 'query_vars', 'discourse_sso_custom_query_vars' );
+
+function discourse_sso_custom_query_vars($vars)
+{
+	 $vars[] = 'discourse_sso';
+	 return $vars;
+}
+
+add_action( 'parse_query', 'discourse_sso_url_redirect' );
+
+function discourse_sso_url_redirect($wp)
+{
+	if ( empty( $wp->query['discourse_sso'] ) ) {
+		return;
+	}
+
 	$discourse_options = DiscourseUtilities::get_options();
 	$is_user_logged_in = is_user_logged_in();
 
-	if ( $is_user_logged_in ) {
-		if ( DiscourseUtilities::user_is_linked_to_sso() ) {
-			return;
-		}
-		$anchor = ! empty( $options['link'] ) ? $options['link'] : __( 'Link your account to Discourse', 'wp-discourse' );
+	if ( ! empty( $_GET['redirect_to'] ) ) {
+		$redirect_to = esc_url( $_GET['redirect_to'] );
 	} else {
-		$anchor = ! empty( $options['login'] ) ? $options['login'] : __( 'Log in with Discourse', 'wp-discourse' );
-	}
-
-	$nonce = DiscourseUtilities::create_nonce( '_discourse_sso' );
-
-	$redirect_to = get_permalink();
-
-	if ( empty( $redirect_to ) ) {
-		$redirect_to = $is_user_logged_in ? admin_url( 'profile.php' ) : home_url( '/' );
-		;
+		$redirect_to = home_url( '/' );
 	}
 
 	$payload = base64_encode(http_build_query(array(
-		'nonce' => $nonce,
+		'nonce' => DiscourseUtilities::create_nonce( '_discourse_sso' ),
 		'return_sso_url' => $redirect_to,
 		)
 	));
@@ -49,9 +45,43 @@ function get_discourse_sso_url( $options = array() ) {
 
 	$sso_login_url = $discourse_options['url'] . '/session/sso_provider?' . http_build_query( $request );
 
+	wp_redirect( $sso_login_url );
+	exit;
+}
+/**
+ * Gets the auth URL for discourse
+ *
+ * @param  array $options anchor, link.
+ *
+ * @return string.
+ */
+function get_discourse_sso_url( $options = array() ) {
+	$is_user_logged_in = is_user_logged_in();
+
+	if ( $is_user_logged_in ) {
+		if ( DiscourseUtilities::user_is_linked_to_sso() ) {
+			return;
+		}
+		$anchor = ! empty( $options['link'] ) ? $options['link'] : __( 'Link your account to Discourse', 'wp-discourse' );
+	} else {
+		$anchor = ! empty( $options['login'] ) ? $options['login'] : __( 'Log in with Discourse', 'wp-discourse' );
+	}
+
+
+	$redirect_to = get_permalink();
+
+	if ( empty( $redirect_to ) ) {
+		$redirect_to = $is_user_logged_in ? admin_url( 'profile.php' ) : home_url( '/' );
+	}
+
+	$sso_login_url = add_query_arg( array(
+		'discourse_sso' => 1,
+		'redirect_to' => $redirect_to,
+	), home_url( '/' ) );
+
 	$anchor = sprintf( '<a href="%s">%s</a>', $sso_login_url, $anchor );
 
-	return apply_filters( 'discourse/sso/provider/login_anchor', $anchor, $sso_login_url, $options, $discourse_options );
+	return apply_filters( 'discourse/sso/provider/login_anchor', $anchor, $sso_login_url, $options );
 }
 
 /**
