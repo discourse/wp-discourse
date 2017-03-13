10136b5eaa0240bb9bdc4006073335b20894472c
scossar <scossar3@gmail.com>
2017-03-13 15:34:35 -0700

    Allow for syncing logout from Discourse when Discourse is the SSO provider

--- lib/sso/query-redirect.php
+++ lib/sso/query-redirect.php
@@ -62,9 +62,25 @@ class QueryRedirect {
 	 * @param  object $wp the wp_query.
 	 */
 	public function discourse_sso_url_redirect( $wp ) {
-		if ( empty( $this->options['sso-client-enabled'] ) || 1 !== intval( $this->options['sso-client-enabled'] ) ||
-		     empty( $wp->query['discourse_sso'] )
-		) {
+		if ( empty( $this->options['sso-client-enabled'] ) || 1 !== intval( $this->options['sso-client-enabled'] ) ) {
+
+			return;
+		}
+
+		/**
+		 * Sync logout from Discourse to WordPress from Adam Capirola : https://meta.discourse.org/t/wordpress-integration-guide/27531.
+		 * To make this work, enter a URL of the form "http://my-wp-blog.com/?request=logout" in the "logout redirect"
+		 * field in your Discourse admin.
+		 */
+		if ( isset( $_GET['request'] ) && 'logout' === $_GET['request'] ) {
+			wp_logout();
+			wp_safe_redirect( $this->options['url']);
+
+			exit;
+		}
+
+		if ( empty( $wp->query['discourse_sso'] ) ) {
+
 			return;
 		}
 
