77d565d2a4aef281bb5ebf6d3136aa0545405117
scossar <scossar3@gmail.com>
2017-03-24 01:49:52 -0700

    Don't show Discourse username nag if WordPress username matches publishing name

--- admin/admin-notice.php
+++ admin/admin-notice.php
@@ -48,10 +48,12 @@ class AdminNotice {
 
 			if ( in_array( $post->post_type, $allowed_post_types, true ) ) {
 				$discourse_username = get_user_meta( get_current_user_id(), 'discourse_username', true );
-				$publish_username = ! empty( $this->options['publish-username'] ) ? $this->options['publish-username'] : 'system';
+				$current_username = wp_get_current_user()->user_login;
+				$publish_username = ! empty( $this->options['publish-username'] ) ? $this->options['publish-username'] : '';
+
 				$profile_page_link = '<a href="' . esc_url( admin_url( '/profile.php' ) ) . '">' . __( 'profile page', 'wp-discourse' ) . '</a>';
 
-				if ( empty( $discourse_username ) ) {
+				if ( empty( $discourse_username ) && $current_username !== $publish_username ) {
 				    $username_not_set_notice = sprintf(
 				    	// translators: Discourse username_not_set notice. Placeholder: discourse_username.
 					__( '<div class="notice notice-error is-dismissible"><p>You have not set your Discourse username. Any posts you publish to Discourse will be published under the system default username \'%1$s\'. To stop seeing this notice, please visit your %2$s and set your Discourse username.</p></div>', 'wp-discourse' ), $publish_username, $profile_page_link );
