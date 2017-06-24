239b36eec92f05a2d88578e6f530e04920e1adc9
scossar <scossar3@gmail.com>
2017-06-24 15:09:38 -0700

    Drop table wpdc_topic_blog in uninstall hook

--- uninstall.php
+++ uninstall.php
@@ -9,7 +9,7 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-$discourse_options = get_option( 'discourse_option_groups' );
+$discourse_options   = get_option( 'discourse_option_groups' );
 $discourse_options[] = 'discourse_configurable_text_backup';
 $discourse_options[] = 'discourse_version';
 $discourse_options[] = 'discourse_option_groups';
@@ -21,8 +21,20 @@ foreach ( $discourse_options as $option ) {
 	delete_site_option( $option );
 }
 
+delete_option( 'wpdc_discourse_categories' );
+
+delete_site_option( 'wpdc_site_url' );
+delete_site_option( 'wpdc_site_api_key' );
+delete_site_option( 'wpdc_site_publish_username' );
+delete_site_option( 'wpdc_site_use_discourse_webhook' );
+delete_site_option( 'wpdc_site_webhook_sync_notification' );
+delete_site_option( 'wpdc_site_multisite_configuration' );
+delete_site_option( 'wpdc_site_sso_secret' );
+delete_site_option( 'wpdc_site_enable_sso' );
+delete_site_option( 'wpdc_site_sso_client_enabled' );
+delete_site_option( 'wpdc_topic_blog_db_version' );
+
 global $wpdb;
 $wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}discourse_nonce" );
-// Todo: delete multisite database table
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->base_prefix}wpdc_topic_blog" );
 
-delete_option( 'wpdc_discourse_categories' );
