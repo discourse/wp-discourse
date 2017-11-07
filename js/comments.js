0d941cd7c1012851d81075511e31349f9ec9ab47
scossar <scossar3@gmail.com>
2017-11-06 16:41:46 -0800

    Comment out a.mention fix

--- js/comments.js
+++ js/comments.js
@@ -12,6 +12,7 @@ jQuery( document ).ready(function() {
 			url = 'https://www.youtube.com/watch?v=' + id;
 		jQuery( this ).replaceWith( '<a href="' + url + '">' + url + '</a>' );
 	});
+	// Todo: this may need to stay as a fallback for sites that don't have lib-sml.
 	/*jQuery( 'a.mention' ).each(function() {
 		jQuery( this ).attr( 'href', discourse.url + jQuery( this ).attr( 'href' ) );
 	});*/
