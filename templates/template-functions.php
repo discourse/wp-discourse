d3f2848f3f03c13478bddbbd20452288fc0a8073
scossar <scossar3@gmail.com>
2017-11-06 16:44:56 -0800

    Add convert_relative_urls_to_absolute function

--- templates/template-functions.php
+++ templates/template-functions.php
@@ -9,6 +9,7 @@ namespace WPDiscourse\Templates;
 
 /**
  * Class TemplateFunctions
+ * Todo: there's no good reason for these to be static functions.
  */
 class TemplateFunctions {
 
@@ -56,5 +57,52 @@ class TemplateFunctions {
 		$replace = "<img src=\"{$url}$1";
 
 		return preg_replace( $search, $replace, $content );
+	}
+
+	public static function convert_relative_urls_to_absolute( $url, $content ) {
+		libxml_use_internal_errors( true );
+
+		$doc = new \DOMDocument( '1.0', 'utf-8' );
+		$html = $html = '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body><div id="inner-content">' . $content . '</div></body></html>';
+		$doc->loadHTML( $html );
+
+		// Mentions and hashtags.
+		$links = $doc->getElementsByTagName( 'a' );
+		foreach ( $links as $link ) {
+			$href= $link->getAttribute( 'href' );
+			$url_parts = parse_url( $href );
+
+			if ( ! isset( $url_parts['host']) || $url_parts['host'] === '' ) {
+				$link->setAttribute( 'href', $url . $href );
+			}
+		}
+
+		// Images, emojis etc.
+		$images = $doc->getElementsByTagName( 'img' );
+		foreach( $images as $image ) {
+			$src = $image->getAttribute( 'src' );
+			$url_parts = parse_url( $src );
+
+			if ( ! isset( $url_parts['host']) || $url_parts['host'] === '' ) {
+				$image->setAttribute( 'src', $url . $src );
+			}
+		}
+
+		$inner_html = self::inner_html($doc->getElementById('inner-content'));
+
+		libxml_clear_errors();
+
+		return $inner_html;
+	}
+
+	public static function inner_html( \DOMElement $element ) {
+		$doc = $element->ownerDocument;
+		$html = '';
+
+		foreach ( $element->childNodes as $node ) {
+			$html .= $doc->saveHTML( $node );
+		}
+
+		return $html;
 	}
 }
