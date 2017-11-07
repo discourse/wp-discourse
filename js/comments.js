1e05b8196c30ffbf8f835645df990319edebb838
scossar <scossar3@gmail.com>
2017-11-07 10:06:18 -0800

    Remove .mention fix from comments.js

--- js/comments.js
+++ js/comments.js
@@ -12,8 +12,4 @@ jQuery( document ).ready(function() {
 			url = 'https://www.youtube.com/watch?v=' + id;
 		jQuery( this ).replaceWith( '<a href="' + url + '">' + url + '</a>' );
 	});
-	// Todo: this may need to stay as a fallback for sites that don't have lib-sml.
-	/*jQuery( 'a.mention' ).each(function() {
-		jQuery( this ).attr( 'href', discourse.url + jQuery( this ).attr( 'href' ) );
-	});*/
 });
