419ed25b724fee971519dad69750b5e99ceac58d
scossar <scossar3@gmail.com>
2017-11-28 15:57:15 -0800

    Remove console.log statements

--- js/load-comments.js
+++ js/load-comments.js
@@ -5,13 +5,11 @@
                 $commentArea = $('.wpdc-comments'),
                 postId = $commentArea.data('post-id');
 
-            console.log('post id', postId);
             $.ajax(
                 {
                     url: commentsURL + '?post_id=' + postId,
                     success: function (response) {
                         if (0 !== response) {
-                            console.log('response', response);
                             $commentArea.html( response );
                         }
                     }
