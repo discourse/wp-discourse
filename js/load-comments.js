fd440566c295be21412b0bfe8a55da7a903f4972
scossar <scossar3@gmail.com>
2017-12-10 12:57:08 -0800

    Changes for wordpress codesniffer

--- js/load-comments.js
+++ js/load-comments.js
@@ -1,20 +1,24 @@
-(function ($) {
-    $(document).ready(
-        function () {
-            var commentsURL = wpdc.commentsURL,
-                $commentArea = $('.wpdc-comments'),
-                postId = $commentArea.data('post-id');
+/**
+ * Loads Discourse comments into the .wpdc-comments div.
+ */
 
-            $.ajax(
-                {
-                    url: commentsURL + '?post_id=' + postId,
-                    success: function (response) {
-                        if (0 !== response) {
-                            $commentArea.html( response );
-                        }
-                    }
-                }
-            );
-        }
-    )
-})(jQuery);
+(function ($) {
+	$( document ).ready(
+		function () {
+			var commentsURL  = wpdc.commentsURL,
+				$commentArea = $( '.wpdc-comments' ),
+				postId       = $commentArea.data( 'post-id' );
+
+			$.ajax(
+				{
+					url: commentsURL + '?post_id=' + postId,
+					success: function (response) {
+						if (0 !== response) {
+							$commentArea.html( response );
+						}
+					}
+				}
+			);
+		}
+	)
+})( jQuery );
