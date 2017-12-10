307529a3f9d330e429f48c2260752228750e1002
scossar <scossar3@gmail.com>
2017-12-10 13:35:31 -0800

    Escape post_id data parameter

--- templates/ajax-comments.php
+++ templates/ajax-comments.php
@@ -1,5 +1,10 @@
 <?php
+/**
+ * The template for when Discourse comments are being loaded with ajax.
+ *
+ * @package WPDiscourse
+ */
 
 global $post;
 
-echo '<div id="wpdc-comments" data-post-id="' . $post->ID . '"></div>';
+echo '<div id="wpdc-comments" data-post-id="' . esc_attr( $post->ID ) . '"></div>';
