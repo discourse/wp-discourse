cce1ffa625ebe7c827a1a211891ae647fbc465fd
scossar <scossar3@gmail.com>
2018-11-25 16:23:31 -0800

    Handle wpdc_topic_tags being saved as a string

--- lib/discourse-publish.php
+++ lib/discourse-publish.php
@@ -192,6 +192,10 @@ class DiscoursePublish {
 		$category         = isset( $publish_post_category ) ? $publish_post_category : $default_category;
 		$category         = apply_filters( 'wpdc_publish_post_category', $category, $post_id );
 		$tags             = get_post_meta( $post_id, 'wpdc_topic_tags', true );
+		// Todo: this is for the Gutenberg API.
+		if( ! is_array( $tags ) ) {
+			$tags = explode( ',', $tags);
+		}
 		$tags_param       = $this->tags_param( $tags );
 
 		// The post hasn't been published to Discourse yet.
