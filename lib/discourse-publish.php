1f1ef333c065875c9ed7a8667875c0dd6cf55c0f
scossar <scossar3@gmail.com>
2018-12-09 16:16:10 -0800

    Check if the publish_post_category is empty

--- lib/discourse-publish.php
+++ lib/discourse-publish.php
@@ -189,10 +189,10 @@ class DiscoursePublish {
 		$publish_post_category = get_post_meta( $post_id, 'publish_post_category', true );
 		// This would be used if a post is published through XML-RPC.
 		$default_category = isset( $options['publish-category'] ) ? $options['publish-category'] : '';
-		$category         = isset( $publish_post_category ) ? $publish_post_category : $default_category;
+		$category         = ! empty( $publish_post_category ) ? $publish_post_category : $default_category;
 		$category         = apply_filters( 'wpdc_publish_post_category', $category, $post_id );
 		$tags             = get_post_meta( $post_id, 'wpdc_topic_tags', true );
-		// For Gutenberg, tags are being set through the API. For this case, it's easier to handle the data as a string.
+		// For the Block Editor, tags are being set through the API. For this case, it's easier to handle the data as a string.
 		if ( ! is_array( $tags ) ) {
 			$tags = explode( ',', $tags );
 		}
