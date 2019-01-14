170bf0c2181c80cc4b29dde50c1bd1a6e20ba9a1
Arkshine <github@arkshine.me>
2019-01-14 19:39:42 +0100

    Honor hide-discourse-name-field option on publisher's username retrieval

--- lib/discourse-publish.php
+++ lib/discourse-publish.php
@@ -180,7 +180,11 @@ class DiscoursePublish {
 		$featured = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'full' );
 		$baked    = str_replace( '{featuredimage}', '![image](' . $featured['0'] . ')', $baked );
 
-		$username = get_the_author_meta( 'discourse_username', $current_post->post_author );
+		$username = null;
+		if ( empty( $options['hide-discourse-name-field'] ) ) {
+			$username = get_the_author_meta( 'discourse_username', $current_post->post_author );
+		}
+
 		if ( ! $username || strlen( $username ) < 2 ) {
 			$username = $options['publish-username'];
 		}
