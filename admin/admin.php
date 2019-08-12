1e99ef481d04c24428b245a036ff7e93195b90bd
scossar <scossar3@gmail.com>
2019-08-12 13:41:07 -0700

    Remove support for the classic editor

--- admin/admin.php
+++ admin/admin.php
@@ -21,7 +21,6 @@ if ( is_admin() ) {
 	require_once __DIR__ . '/sso-settings.php';
 	require_once __DIR__ . '/webhook-settings.php';
 	require_once __DIR__ . '/admin-notice.php';
-	require_once __DIR__ . '/meta-box.php';
 
 	$form_helper  = FormHelper::get_instance();
 	$options_page = OptionsPage::get_instance();
@@ -37,7 +36,6 @@ if ( is_admin() ) {
 	new WebhookSettings( $form_helper );
 	new SettingsValidator();
 	new AdminNotice();
-	new MetaBox();
 
 	add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\enqueue_admin_scripts' );
 	if ( is_multisite() ) {
