9b821cfd17b436d185c0e58d1522e9dbefc75852
scossar <scossar3@gmail.com>
2019-10-04 13:18:47 -0700

    Display error message returned from Discourse

--- admin/meta-box.php
+++ admin/meta-box.php
@@ -611,16 +611,10 @@ class MetaBox {
 
 				return 'handled_error';
 
-			case 'The tag you selected cannot be used':
-				esc_html_e( 'The tags you selected cannot be used in the Discourse category. Try removing the tags and republishing your post.', 'discourse-integration' );
-				echo '<hr>';
-
-				return 'unhandled_error';
-
 			default:
 				$action = $already_published ? __( 'updating', 'wp-discourse' ) : __( 'publishing', 'wp-discourse' );
 				// translators: Publishing error message. Placeholder: publishing or updating.
-				$message = sprintf( __( 'There has been an error %1s your post. Please try again.', 'wp-discourse' ), $action );
+				$message = sprintf( __( 'There has been an error %1s your post:', 'wp-discourse' ), $action ) . " $publishing_error.";
 				echo wp_kses_post( $message );
 
 				echo '<hr>';
