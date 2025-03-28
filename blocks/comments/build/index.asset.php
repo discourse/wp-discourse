f714f0c8f8210c5a9cd7c4cbe4e5726ca707db80
Angus McLeod <angus@mcleod.org.au>
2025-03-28 18:33:53 +0100

    Re-generate comments js build

--- blocks/comments/build/index.asset.php
+++ blocks/comments/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '08c077a71fb7b2f93917');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '857b20870324aa144770');
